ae0589bf8435b7b04d4843aef2230327c762c91c
Roshan-R <roshanr2001@gmail.com>
2022-05-07 20:15:11 +0530

    fixed token login

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from . import serializers
@@ -13,7 +14,9 @@ class UserListView(generics.ListAPIView):
     # lookup_field = 'email'
     # queryset = models.CustomUser.objects.all()
     # serializer_class = serializers.UserSerializer
+
 class UserView(APIView):
+    authentication_classes = (TokenAuthentication,)
     def get(self, request, format=None):
         user = request.user
         if user.id:
