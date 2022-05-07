b044d7b7707c9bfa3ceac777528834eefd546a32
Roshan-R <roshanr2001@gmail.com>
2022-05-07 11:13:56 +0530

    added new fields as specified

--- user/serializers.py
+++ user/serializers.py
@@ -14,15 +14,25 @@ class UserSerializer(RegisterSerializer):
     referral_code = serializers.CharField(max_length=7, read_only=True)
     points = serializers.IntegerField(default=0, read_only=True)
 
+    college_district = serializers.CharField(required=True, max_length=100)
+    year = serializers.CharField(max_length=2)
+    department = serializers.CharField(required=True, max_length=100)
+    phonenumber2 = serializers.CharField(max_length=100)
+
     ordering = ('email',)
 
     def get_cleaned_data(self):
-        # data_dict = super(UserSerializer(), self).get_cleaned_data()
         data_dict = super().get_cleaned_data()
         data_dict['college_name'] = self.validated_data.get('college_name', '')
         data_dict['name'] = self.validated_data.get('name', '')
         data_dict['phonenumber'] = self.validated_data.get('phonenumber', '')
-        data_dict['referral_code'] = self.validated_data.get('referral_code', '')        
+        data_dict['referral_code'] = self.validated_data.get('referral_code', '')
+
+        data_dict['college_district'] = self.validated_data.get('college_district', '')
+        data_dict['year'] = self.validated_data.get('year', '')
+        data_dict['department'] = self.validated_data.get('department', '')
+        data_dict['phonenumber2'] = self.validated_data.get('phonenumber2', '')
+
         return data_dict
 
     def save(self, request):
@@ -38,5 +48,10 @@ class UserSerializer(RegisterSerializer):
         user.phonenumber = self.data.get('phonenumber')
         user.points = 0
         user.referral_code = res
+
+        user.college_district = self.data.get('college_district')
+        user.year = self.data.get('year')
+        user.department = self.data.get('department')
+        user.phonenumber2 = self.data.get('phonenumber2')
         user.save()
         return user
